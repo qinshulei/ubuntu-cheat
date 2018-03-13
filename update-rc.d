a32606e965f8aa2cbc7c53b6e4e6bb3b872bfc43
qinshulei <527072230@qq.com>
2018-03-13 12:52:02 +0800

    update

--- update-rc.d
+++ update-rc.d
@@ -1,2 +1,10 @@
 # disable apache2
 update-rc.d apache2 disable
+
+update-rc.d nfs-kernel-server disable
+update-rc.d ceph disable
+update-rc.d snmpd disable
+update-rc.d openvpn disable
+update-rc.d apache-htcacheclean disable
+update-rc.d postgresql disable
+update-rc.d virtualbox disable
